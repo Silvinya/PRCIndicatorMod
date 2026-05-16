4729be84dd2a37f01e6dd16614e640922950269f
Silvinya <crunch@hetnet.nl>
2026-05-16 15:45:21 +0200

    Initial commit

---
+++ src/RCIndicator/Patches/GUIManagerPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace RCIndicator.Patches
+{
+    [HarmonyPatch]
+    internal class GUIManagerPatch
+    {
+        [HarmonyPatch(typeof(Item))]
+        [HarmonyPostfix]
+
+        private static void RCPatch(ref Color ___reticleColorHighlight)
+        {
+            
+        }
+
+    }
+}
